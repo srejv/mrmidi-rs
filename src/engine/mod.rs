c857f2f62e6955bbcb05b781069505f92ff8a1c1
Oscar Dragén <oscar.dragen@gmail.com>
2022-10-18 11:01:48 +0200

    feat: Add midi and audio to mod

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,8 +1,10 @@
 pub mod app_state;
+pub mod audio;
 pub mod built_in_uniforms;
 pub mod color_picker;
 pub mod internal_uniform;
 pub mod mesh;
+pub mod midi;
 pub mod shader_data;
 pub mod track;
 pub mod uniform;
